161a4cb7973ff4a88125332a03356af87029fa63
fancy <fancy.zhang@ximalaya.com>
2018-09-03 10:52:54 +0800

    增加题目：24

---
+++ medium_code/Linked_List/24.swapnodesinpairs.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+@author:nali 
+@file: 24.swapnodesinpairs.py 
+@time: 2018/9/3/上午10:23
+@software: PyCharm
+"""
+
+"""
+Given a linked list, swap every two adjacent nodes and return its head.
+
+Example:
+
+Given 1->2->3->4, you should return the list as 2->1->4->3.
+Note:
+
+Your algorithm should use only constant extra space.
+You may not modify the values in the list's nodes, only nodes itself may be changed.
+
+"""
+
+import sys
+
+reload(sys)
+sys.setdefaultencoding("utf8")
+
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+def swapPairs(head):
+    """
+    :type head: ListNode
+    :rtype: ListNode
+    """
+    now = head
+    while now and now.next:
+        suff = now.next
+        tmp = now.val
+        now.val = suff.val
+        suff.val = tmp
+        now = suff.next
+    return head
+
+
+if __name__ == "__main__":
+    pass
