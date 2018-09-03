39c572f33f1b189980bb45a4edc90eaddcc351fc
fancy <fancy.zhang@ximalaya.com>
2018-09-03 16:24:26 +0800

    增加题目：92

--- medium_code/Linked_List/92.reverselinkedlistII.py
+++ medium_code/Linked_List/92.reverselinkedlistII.py
@@ -6,6 +6,7 @@
 @time: 2018/9/3/下午1:52
 @software: PyCharm
 """
+from medium_code.Linked_List import ListNode
 
 """
 Reverse a linked list from position m to n. Do it in one-pass.
@@ -22,6 +23,40 @@ import sys
 
 reload(sys)
 sys.setdefaultencoding("utf8")
+
+
+def reverseBetween(head, m, n):
+    """
+    :type head: ListNode
+    :type m: int
+    :type n: int
+    :rtype: ListNode
+    """
+    if m == n:
+        return head
+
+    nhead = ListNode(None)
+    nhead.next = head
+    count = 0
+    now = nhead
+    flag = None
+    stack = []
+    while now:
+        now = now.next
+        count += 1
+        if count == m:
+            flag = now
+            stack.append(now.val)
+        elif count > m and count <= n:
+            stack.append(now.val)
+        elif count > m:
+            break
+
+    for i in range(stack.__len__() - 1, -1, -1):
+        flag.val = stack[i]
+        flag = flag.next
+    return nhead.next
+
 
 if __name__ == "__main__":
     pass
