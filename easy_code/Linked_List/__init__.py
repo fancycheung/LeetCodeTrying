58fdde014af2b5e73c93f64f208a877f90cfce4c
fancy <fancy.zhang@ximalaya.com>
2018-09-03 10:03:17 +0800

    文件目录改版

---
+++ easy_code/Linked_List/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+@author:nali 
+@file: __init__.py.py 
+@time: 2018/9/3/上午10:00
+@software: PyCharm
+"""
+
+"""
+链表相关的题目
+"""
+
+import sys
+
+reload(sys)
+sys.setdefaultencoding("utf8")
+
+if __name__ == "__main__":
+    pass
